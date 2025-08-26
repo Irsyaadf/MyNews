cb1fc3130e7b3a0063e3ca87e2055acfe5da5021
Rezvanankbaik <rezvanhaidarawwalawwal@gmail.com>
2025-08-26 11:16:17 +0700

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       <Router>
         <Routes>
-          <Route path="/home" element={<HomePage />}/>
+          <Route path="/" element={<HomePage />}/>
         </Routes>
       </Router>
     </div>
